fd9e645066d53884006e51fa48912ab3e1d007fb
Anton <TheAlmightyYudHay@users.noreply.github.com>
2017-11-29 21:34:03 +0200

    Change server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require('http'),
+      fs   = require('fs'),
+      path = require('path'),
+      mime = require('mime'),
+      cache = {};
+
+const send404 = (response) => {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('Error 404: resource not found.');
+    response.end();
+};
+
+const sendFile = (response, filePath, fileContents) => {
+    response.writeHead(
+        200,
+        {'content-type': mime.lookup(path.basename(filePath))}
+    );
+    response.end(fileContents);
+};
+
+const serverStatic = (response, cache, absPath) => {
+    if (cache[absPath]) {
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, (exists) => {
+            if (exists) {
+                fs.readFile(absPath, (err, data) => {
+                    if (err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                })
+            } else {
+                send404(response);
+            }
+        });
+    }
+};
